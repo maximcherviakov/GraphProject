a1343353c063b265dc3f67080f114738d46c4dc5
Maxim <cherviakovmaxim@gmail.com>
2022-10-04 20:32:57 +0300

    Graph library was added

--- src/main/java/com/example/graphproject/controller/HelloController.java
+++ src/main/java/com/example/graphproject/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.graphproject;
+package com.example.graphproject.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.example.graphproject {
 
     opens com.example.graphproject to javafx.fxml;
     exports com.example.graphproject;
+    exports com.example.graphproject.controller;
+    opens com.example.graphproject.controller to javafx.fxml;
 }
