a51119336152776fc8bd08e2e4e18f891ab49aa9
maxim <cherviakovmaxim@gmail.com>
2022-10-19 21:04:19 +0300

    GraphInstance class changes

--- src/main/java/com/example/graphproject/controller/MainController.java
+++ src/main/java/com/example/graphproject/controller/MainController.java
@@ -1,16 +1,37 @@
 package com.example.graphproject.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import com.example.graphproject.model.*;
 
+import java.util.Random;
+
 public class MainController {
+    private final double RADIUS_FIR_CIRCLE = 20;
+    private final Paint COLOR_FOR_CIRCLE_BACKGROUND = Color.web("#F1F1F1");
+    private final Paint COLOR_FOR_CIRCLE_STROKE = Color.web("#3299D9");
     @FXML
     AnchorPane graphCanvas;
 
     @FXML
     protected void onAddVertexButtonClick() {
-        graphCanvas.getChildren().add(new Circle(200, 200, 200));
+        graphCanvas.getChildren().add(addNode(Math.random() * graphCanvas.getHeight(), Math.random() * graphCanvas.getWidth()));
+    }
+
+    @FXML
+    protected void clearGraph() {
+        graphCanvas.getChildren().clear();
+    }
+
+    private Circle addNode(double x, double y) {
+        Circle circle = new Circle(x, y, RADIUS_FIR_CIRCLE, COLOR_FOR_CIRCLE_BACKGROUND);
+        circle.setStrokeWidth(4);
+        circle.setStroke(COLOR_FOR_CIRCLE_STROKE);
+        return circle;
     }
 }
--- src/main/java/com/example/graphproject/model/GraphInstance.java
+++ src/main/java/com/example/graphproject/model/GraphInstance.java
@@ -3,24 +3,39 @@ package com.example.graphproject.model;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
+import org.jgrapht.nio.GraphImporter;
+
+import java.util.HashMap;
+import java.util.Objects;
 
 public class GraphInstance {
-    private static Graph<Integer, DefaultEdge> graph;
+    private static SimpleGraph<Integer, DefaultEdge> graph = new SimpleGraph<>(DefaultEdge.class);
+    private static HashMap<Integer, Point> mapOfPoints = new HashMap<>();
+    private static GraphInstance instance;
 
     private GraphInstance() {
 
     }
 
-    public static Graph<Integer, DefaultEdge> getInstance() {
-        if (graph == null) {
-            graph = new SimpleGraph<>(DefaultEdge.class);
+    public static GraphInstance getInstance() {
+        if (instance == null) {
+            instance = new GraphInstance();
         }
+        return instance;
+    }
+
+    public Graph<Integer, DefaultEdge> getGraph() {
         return graph;
     }
 
-    public void addVertex(Integer integer) throws Exception {
-        if (!graph.containsVertex(integer)) {
-            graph.addVertex(integer);
+    public Point getCoordinatesByVertexValue(Integer value) {
+        return mapOfPoints.get(value);
+    }
+
+    public void addVertex(Integer value) throws Exception {
+        if (!graph.containsVertex(value)) {
+            graph.addVertex(value);
+            mapOfPoints.put(value, new Point(Math.random(), Math.random()));
         } else {
             throw new Exception("Here is a duplicate of a vertex!");
         }
@@ -30,7 +45,6 @@ public class GraphInstance {
         if (!graph.containsEdge(integer1, integer2)) {
             graph.addEdge(integer1, integer2);
         } else {
-
             throw new Exception("Here is a duplicate of an edge!");
         }
     }
@@ -46,6 +60,98 @@ public class GraphInstance {
 
         for (Integer i : graph.vertexSet()) {
             System.out.println(i);
+        }
+    }
+
+    private class Point {
+        private double x;
+        private double y;
+
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public void setX(double x) {
+            this.x = x;
+        }
+
+        public double getY() {
+            return y;
+        }
+
+        public void setY(double y) {
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return "Point{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    '}';
+        }
+    }
+
+    private class Vertex {
+        private int value;
+        private double x;
+        private double y;
+
+        public Vertex(int value, double x, double y) {
+            this.value = value;
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public void setX(double x) {
+            this.x = x;
+        }
+
+        public double getY() {
+            return y;
+        }
+
+        public void setY(double y) {
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Vertex vertex = (Vertex) o;
+            return value == vertex.value;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(value);
+        }
+
+        @Override
+        public String toString() {
+            return "Vertex{" +
+                    "value=" + value +
+                    ", x=" + x +
+                    ", y=" + y +
+                    '}';
         }
     }
 }
