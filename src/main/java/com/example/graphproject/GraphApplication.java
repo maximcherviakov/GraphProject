b84c6d3cb71bcb015c0722fee726a2dc74baee43
BohdanRybalka <021102rb@gmail.com>
2022-10-19 21:06:46 +0300

    Graph Operations class for import/export operations with graph was created

--- src/main/java/com/example/graphproject/GraphApplication.java
+++ src/main/java/com/example/graphproject/GraphApplication.java
@@ -32,27 +32,7 @@ public class GraphApplication extends Application {
     public static void main(String[] args) {
         launch();
 
-        Graph<Integer, DefaultEdge> graph = GraphInstance.getInstance();
-
-        graph.addVertex(0);
-        graph.addVertex(1);
-        graph.addVertex(2);
-        graph.addVertex(3);
-        graph.addVertex(4);
-
-        graph.addEdge(0,1);
-        graph.addEdge(0,2);
-        graph.addEdge(0,3);
-        graph.addEdge(1,3);
-        graph.addEdge(1,4);
-        graph.addEdge(2,3);
-        graph.addEdge(3,4);
-
-        System.out.println(graph.toString());
-
-        graph.removeAllEdges(0,1);
 
 
-        System.out.println(graph.toString());
     }
 }
